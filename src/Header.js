626b3af095fbba75a69ebe38f115ce33148f6b03
Slava Polishchuk <dizerllc@gmail.com>
2020-08-19 13:31:27 +0300

    todo plan

---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Header.css';
+
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        {/* Avatars from logged in user */}
+        {/* Time icon */}
+      </div>
+      <div className="header__search">
+      {/* Search icon */}
+      </div>
+      <div className="header__right">
+        {/* help icon */}
+      </div>
+    </div>
+  )
+}
+
+export default Header
