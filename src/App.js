a764d015a37d69ed81d54eb84904a8fff2437def
Slava Polishchuk <dizerllc@gmail.com>
2020-08-19 14:31:47 +0300

    header styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>something</h1>
-
+      <Header />
+      <div className="app__body">
+        {/* Sidebar */}
+        {/* React-Router -> Chat screen */}
+      </div>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import './Header.css';
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/Help";
 
 
 function Header() {
   return (
     <div className="header">
       <div className="header__left">
-        {/* Avatars from logged in user */}
-        {/* Time icon */}
+        <Avatar 
+          className="header__avatar"
+          alt="Slava"
+          src="{user?.photoURL}"
+        />
+        <AccessTimeIcon />
       </div>
       <div className="header__search">
-      {/* Search icon */}
+        <SearchIcon />
+        <input placeholder="Search" />
       </div>
       <div className="header__right">
-        {/* help icon */}
+        <HelpOutlineIcon />
       </div>
     </div>
   )
